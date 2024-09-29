f09e098a9372624107358d09fac8ab51b8afe671
shivi028 <tiwarishivi028@gmail.com>
2024-09-29 10:45:52 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import ContributionGraph from "@/components/Contribution Graph/Contribution";
 import Hero from "@/components/Hero/Hero";
-// import Navbar from "@/components/Navbar/Navbar";
-// import Image from "next/image";
 
 export default function Home() {
   return (
